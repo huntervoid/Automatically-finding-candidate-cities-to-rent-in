032b53fd3560aa3e416d268953281f2d954adc2e
huntervoid <132970909+huntervoid@users.noreply.github.com>
2023-05-18 20:36:44 -0700

    filter results works. i can return  all cities neighbors to centers

--- get_distances.py
+++ get_distances.py
@@ -8,6 +8,8 @@ import re
 import math
 import os
 
+import copy
+
 import locale
 
 import json
@@ -89,123 +91,176 @@ class DistanceMatrixTest():
         # print(r.text)
         return r
 
-    def filter_results(self, r):
-        max_dist_from_center = 32 # km
+    # def filter_results(self, r):
+    #     max_dist_from_center = 32 # km
+    #     city_neighbors = []
+    #     pp = pprint.PrettyPrinter(indent=4)
+    #     r_dict = json.loads(r.text)
+    #     # pp.pprint(r_dict)
+    #     destinations = r_dict["destination_addresses"]
+    #     origins = r_dict["origin_addresses"]
+    #     # pp.pprint(origins)
+    #     # pp.pprint(len(origins))
+    #     # pp.pprint(destinations)
+    #     rows = r_dict["rows"]
+    #     for i in range(len(origins)):
+    #         elements = rows[i]['elements']
+    #         for e in elements:
+    #             try:
+    #                 s = (e["distance"]['text']).split()
+    #             except:
+    #                 continue
+    #             # distance_from_SF = e["distance"]['text']
+    #             # distance_from_SF = float(s[0])
+    #             distance_from_center = locale.atof(s[0])
+    #             # pp.pprint(distance_from_SF)
+    #             if distance_from_center <= max_dist_from_center:
+    #                 city_neighbors.append(r_dict["origin_addresses"][i])
+    #     # pp.pprint(city_neighbors)
+    #     return city_neighbors
+
+    # def filter_results(self, distances):
+    #     max_dist_from_center = 150.0 # km
+    #     min_hits_num = 3
+    #     city_neighbors = []
+    #     pp = pprint.PrettyPrinter(indent=4)
+    #     distances_bak = copy.deepcopy(distances)
+    #     # pp.pprint(distances_bak)
+    #     distances = distances[1:]
+    #     # pp.pprint(distances_bak)
+    #     for origin in range(len(distances)):
+    #         # if origin == 0:
+    #         #     continue
+    #         num_hits = 0
+    #         for destination in range(len(distances[0])-1):
+    #             destination += 1
+    #             if float(distances[origin][destination]) <= float(max_dist_from_center):
+    #                 # pp.pprint(float(distances[origin][destination]))
+    #                 num_hits += 1
+    #         if num_hits < min_hits_num:
+    #             distances_bak[origin] = []
+    #     return distances_bak
+
+
+    def filter_results(self, distances):
+        max_dist_from_center = 150.0 # km
+        min_hits_num = 3
         city_neighbors = []
         pp = pprint.PrettyPrinter(indent=4)
-        r_dict = json.loads(r.text)
-        # pp.pprint(r_dict)
-        destinations = r_dict["destination_addresses"]
-        origins = r_dict["origin_addresses"]
-        # pp.pprint(origins)
-        # pp.pprint(len(origins))
-        # pp.pprint(destinations)
-        rows = r_dict["rows"]
-        for i in range(len(origins)):
-            elements = rows[i]['elements']
-            for e in elements:
-                try:
-                    s = (e["distance"]['text']).split()
-                except:
-                    continue
-                # distance_from_SF = e["distance"]['text']
-                # distance_from_SF = float(s[0])
-                distance_from_center = locale.atof(s[0])
-                # pp.pprint(distance_from_SF)
-                if distance_from_center <= max_dist_from_center:
-                    city_neighbors.append(r_dict["origin_addresses"][i])
-        # pp.pprint(city_neighbors)
-        return city_neighbors
+        distances_filtered = []
+        distances = distances[1:]
+        for origin in range(len(distances)):
+            # if origin == 0:
+            #     continue
+            num_hits = 0
+            for destination in range(len(distances[0])-1):
+                destination += 1
+                if float(distances[origin][destination]) <= float(max_dist_from_center):
+                    # pp.pprint(float(distances[origin][destination]))
+                    num_hits += 1
+            if num_hits >= min_hits_num:
+                distances_filtered.append(distances[origin])
+        return distances_filtered    
 
-    def compute_distance_matrix(self, origins, destinations):
-        results = foo.test_basic_params(origins, destinations)
+
+    def chunk_origins(self, origins):
+        num_candidate_cities = len(origins)
+        chunk_size = 10
+        num_chunks = math.ceil(num_candidate_cities/chunk_size)
+        leftovers = num_candidate_cities % chunk_size
+        # print(leftovers)
+        chunked_origins = []
+        # all_city_neighbors = []
+        for c in range(num_chunks):
+            if c*chunk_size <= num_candidate_cities-chunk_size-1:
+                chunked_origins.append(origins[c*chunk_size:(c+1)*chunk_size])
+            else:
+                chunked_origins.append(origins[c*chunk_size:c*chunk_size+leftovers])
+        return chunked_origins
+
+    def compute_distance_matrix(self, all_origins, destinations):
         pp = pprint.PrettyPrinter(indent=4)
-        r_dict = json.loads(results.text)
-        destinations = r_dict["destination_addresses"]
-        origins = r_dict["origin_addresses"]
-        rows = r_dict["rows"]
-        distances = [["City"]]
-        for d in destinations:
-            d_city = re.sub("CA|USA|,", "", d)
-            d_city = re.sub(" ","_", d_city)
-            d_city = re.sub("__","", d_city)
-            distances[0].append(d_city)
-        # distance = []
-        # distance = ["City"]
-        # distance.extend(destinations)
-        for i in range(len(origins)):
-            elements = rows[i]['elements']
-            distance = []
-            # o = origins[i].split()
-            # distance.append(origins[i])
-            # distance.append(o)
-            o_city = re.sub("CA|USA|,", "", origins[i])
-            # print(o_city)
-            o_city = re.sub(" ","_", o_city)
-            o_city = re.sub("__","", o_city)
-            # print(o_city)
-            distance = [o_city]
-            for e in elements:
-                try:
-                    # pp.pprint(e)
-                    d = (e["distance"]['text']).split()
-                    # pp.pprint(d)
-                    distance.append(str(locale.atof(d[0])))
-
-                except Exception as exception:
-                    # pp.pprint(d)
-                    print(exception)
-                    continue
-                    # distance.extend by -1
-            distances.append(distance)
+        chunked_origins = self.chunk_origins(all_origins)
+        # pp.pprint(chunked_origins)
         filename = "./distance_matrix.csv"
-        # f = open(filename, "w")
-        with open(filename, 'w') as f:
-            for distance in distances:
-                pp.pprint(' '.join(distance))
-                d_str = ' '.join(distance)
-                # pp.pprint(d_str)
-                f.write(f"{d_str}\n")
-                # pp.pprint(distances, stream=f)
-                # file_pp = pprint.PrettyPrinter(stream = f)
-                # file_pp.pprint(d_str)
-                # f.write(pp.pprint.pformat(distances))
+        distances = [[]]
+        for origins in chunked_origins:
+            # pp.pprint(origins)
+            # pp.pprint(destinations)
+            results = foo.test_basic_params(origins, destinations)
+            # pp = pprint.PrettyPrinter(indent=4)
+            r_dict = json.loads(results.text)
+            # pp.pprint(r_dict)
+            destinations = r_dict["destination_addresses"]
+            origins = r_dict["origin_addresses"]
+            rows = r_dict["rows"]
+            # distances = [[]]
+            if os.path.isfile(filename) == False:
+                distances[0] = ["City"]
+                for d in destinations:
+                    d_city = re.sub("CA|USA|,", "", d)
+                    d_city = re.sub(" ","_", d_city)
+                    d_city = re.sub("__","", d_city)
+                    distances[0].append(d_city)
+            # distance = []
+            # distance = ["City"]
+            # distance.extend(destinations)
+            for i in range(len(origins)):
+                elements = rows[i]['elements']
+                distance = []
+                o_city = re.sub("CA|USA|,", "", origins[i])
+                # print(o_city)
+                o_city = re.sub(" ","_", o_city)
+                o_city = re.sub("__","", o_city)
+                # print(o_city)
+                distance = [o_city]
+                for e in elements:
+                    try:
+                        # pp.pprint(e)
+                        d = (e["distance"]['text']).split()
+                        # pp.pprint(d)
+                        distance.append(str(locale.atof(d[0])))
+
+                    except Exception as exception:
+                        # pp.pprint(d)
+                        print(exception)
+                        continue
+                        # distance.extend by -1
+                # pp.pprint(distances)
+                distances.append(distance)
+                # pp.pprint(distances)
+            # filename = "./distance_matrix.csv"
+            # f = open(filename, "w")
+            if os.path.isfile(filename) == False:
+                with open(filename, 'w') as f:
+                    for distance in distances:
+                        # pp.pprint(' '.join(distance))
+                        d_str = ' '.join(distance)
+                        # pp.pprint(d_str)
+                        f.write(f"{d_str}\n")
+                    f.close()
+            else:
+                with open(filename, 'a') as f:
+                    for distance in distances:
+                        # pp.pprint(' '.join(distance))
+                        d_str = ' '.join(distance)
+                        # pp.pprint(d_str)
+                        f.write(f"{d_str}\n")
+                    f.close()
 
         return distances
-                # distance_from_SF = e["distance"]['text']
-                # distance_from_SF = float(s[0])
 
     def find_neighbors(self, center_city, destinations):
         pp = pprint.PrettyPrinter(indent=4)
-        cities_near_center = []
-        center_name = '_'.join(center_city.split())
-        center_name = center_name.replace(',', '')
-        print(center_name)
-        filename = "./cities_near_" + center_name + ".csv"
-        # Check if file exists
 
-
-        f = open(filename, "w")
-        num_candidate_cities = len(destinations)
-        chunk_size = 15
-        num_chunks = math.ceil(num_candidate_cities/chunk_size)
-        leftovers = num_candidate_cities % chunk_size
-        print(leftovers)
-        evaluate = []
-        all_city_neighbors = []
-        for c in range(num_chunks):
-            if c*chunk_size <= num_candidate_cities-chunk_size-1:
-                evaluate = destinations[c*chunk_size:(c+1)*chunk_size]
-            else:
-                evaluate = destinations[c*chunk_size:c*chunk_size+leftovers]
-            results = foo.test_basic_params(evaluate, center_city)
-            # pp.pprint(results)
-            city_neighbors = foo.filter_results(results)
-            if len(city_neighbors) > 0:
-                # pp.pprint(city_neighbors)
-                write_string = '\n'.join(city_neighbors)
-                print(write_string, file=f)
-                all_city_neighbors.extend(city_neighbors)
+        results = foo.test_basic_params(evaluate, center_city)
+        # pp.pprint(results)
+        city_neighbors = foo.filter_results(results)
+        if len(city_neighbors) > 0:
+            # pp.pprint(city_neighbors)
+            write_string = '\n'.join(city_neighbors)
+            print(write_string, file=f)
+            all_city_neighbors.extend(city_neighbors)
         f.close()
         return all_city_neighbors
 
@@ -213,15 +268,15 @@ class DistanceMatrixTest():
 pp = pprint.PrettyPrinter(indent=4)
 SF = ["San Francisco, USA"]
 attractive_centers = [
-    # "San Francisco, USA",
+    "San Francisco, USA",
     "Half Moon Bay, USA",
-    # "San Jose, USA",
-    # "Fremont, USA",
+    "San Jose, USA",
+    "Fremont, USA",
     "Palo Alto",
-    # "Berkeley, USA",
+    "Berkeley, USA",
     # "Sacramento, USA",
     "San Mateo, USA",
-    # "Oakland, USA"
+    "Oakland, USA"
 ]
 csv_file_path='/Users/huntervoid/programming/Ranking cities in NorCal/cal_cities.csv'
 cal_cities = []
@@ -237,8 +292,10 @@ foo.setUp()
 all_city_neighbors = []
 all_attractive_centers_neighbors = []
 
-dist_mat = foo.compute_distance_matrix(cal_cities[0:10], attractive_centers)
+dist_mat = foo.compute_distance_matrix(cal_cities[0:15], attractive_centers)
 # pp.pprint(dist_mat)
+filtered_cities = foo.filter_results(dist_mat)
+pp.pprint(filtered_cities)
 
 
 # for center in attractive_centers:
